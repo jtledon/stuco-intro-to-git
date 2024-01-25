9ab423e5c8913fab86a213da20ffbd4c8de07e70
Katie Lin <katielin019@gmail.com>
2024-01-24 20:19:17 -0500

    print # of unique chars

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ def main():
     print("Running...")
     word = input("Input text to reverse: ").lower()
     reverse(word)
+    unique(word)
     return
 
 def reverse(word):
     print(word[::-1])
+
+def unique(word):
+    chars = {*word}
+    print(len(chars))
 
 if __name__ == "__main__":
     main()
