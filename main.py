d9b81a17370fff7c7bc3e9bf7167240556eeffca
Katie Lin <katielin019@gmail.com>
2024-01-24 20:13:32 -0500

    add reverse function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
     print("Running...")
+    word = input("Input text to reverse: ")
+    reverse(word)
     return
+
+def reverse(word):
+    print(word[::-1])
 
 if __name__ == "__main__":
     main()
