df5a8b5f376fb9b1129b1e40cf6fc4dbb73a1484
awagner26 <aidan.wagner140@gmail.com>
2024-01-31 13:20:07 -0500

    more frogs

--- frog.py
+++ frog.py
@@ -1,6 +1,10 @@
 def frog(num_frogs):
-    for i in range(num_frogs):
+    i = 0
+    while i < num_frogs:
         print("frog here")
+        i += 1
+        num_frogs += 1
+
 
 if __name__ == "__main__":
     frog(10)
