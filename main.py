378d5e23b5b5435eb9215120f5e079ab3a39c78d
Ryan Lin <rlin2@andrew.cmu.edu>
2024-01-31 15:13:29 -0500

    Intuitive Even-Odd Func

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 def main():
-    print("Running...")
-    return
+	num = 987654321
+	print("Running...")
+   	if num == 0 return "Even"
+    	if num == 1 return "Odd"
+	if num == 2 return "Even"
+    	if num == 3 return "Odd"
+    	if num == 4 return "Even"
+    	if num == 5 return "Odd"
+        if num == 6 return "Even"
+        if num == 7 return "Odd"
+        if num == 8 return "Even"
+        if num == 9 return "Odd"
+        if num == 10 return "Even"
+        if num == 11 return "Odd"
+        if num == 12 return "Even"
+        if num == 13 return "Odd"
+        if num == 14 return "Even"
+        if num == 15 return "Odd"
+        if num == 16 return "Even"
+        if num == 17 return "Odd"
+        if num == 18 return "Even"
+        if num == 19 return "Odd"
+        if num == 20 return "Even"
+        if num == 21 return "Odd"
+        if num == 22 return "Even"
+        if num == 23 return "Odd"
+        if num == 24 return "Even"
+        if num == 25 return "Odd"
+        if num == 26 return "Even"
+        if num == 27 return "Odd"
+        if num == 28 return "Even"
+        if num == 29 return "Odd"
+    	return
 
 if __name__ == "__main__":
-    main()
+    	main()
