9572bfbef5bd102f42d2f08f2d70b9c41d329893
unknown <peterx@andrew.cmu.edu>
2024-02-01 21:06:13 +0000

    test

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-def main():
-    print('''wind rattles the trees, 
-            yet no leaves remain,
-            to fall''')
-    return
-
-if __name__ == "__main__":
-    main()
+some changes made
